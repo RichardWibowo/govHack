c8adf33eac65e6846ef9bf8a99c53b818895705e
Richard Wibowo <richardmulyawan@hotmail.com>
2023-08-20 16:32:36 +1000

    Update Hero.tsx

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero({}: Props) {
       <div className='z-20 h-[60%] pb-30'>
       <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>The Future Is Here </h2>
       
-      <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+      <h1 className='text-5xl lg:text-6xl font-semibold px-10 pb-40'>
         <span className='mr-3'>{text}</span>
         <Cursor cursorColor='#000000' />
       </h1>
